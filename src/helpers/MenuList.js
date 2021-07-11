cc4376ea6060a08f7121b954629971a21eced355
Jeshan Maharjan <jeshan.marz@gmail.com>
2021-07-11 23:44:55 +0545

    pizzaria simple react project

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,39 @@
+import Pepperoni from '../assets/pepperoni.jpg';
+import Margherita from '../assets/margherita.jpg';
+import JikkzTechSpecial from '../assets/pedrotechspecial.jpg';
+import Vegan from '../assets/vegan.jpg';
+import Pineapple from '../assets/pineapple.jpg';
+import Expensive from '../assets/expensive.jpg';
+
+export const MenuList = [
+    {
+        name: "Pepperoni Pizza",
+        image: Pepperoni,
+        price: 129.9,
+    },
+    {
+        name: "Margherita Pizza",
+        image: Margherita,
+        price: 119.9,
+    },
+    {
+        name: "JikkzTech Special Pizza",
+        image: JikkzTechSpecial,
+        price: 565,
+    },
+    {
+        name: "Vegan Pizza",
+        image: Vegan,
+        price: 179.9,
+    },
+    {
+        name: "Pineapple Pizza",
+        image: Pineapple,
+        price: 49.9,
+    },
+    {
+        name: "Very Expensive Pizza",
+        image: Expensive,
+        price: 1997.99,
+    },
+];
---
+++ src/pages/Contact.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import PizzaLeft from '../assets/pizzaLeft.jpg';
+import '../styles/Contact.css';
+
+function Contact() {
+    return (
+        <div className="contact">
+           <div className="leftSide" style={{ backgroundImage: `url(${PizzaLeft})` }}></div>
+           <div className="rightSide">
+                <h1>Contact Us</h1>
+                <form action="" method="POST" id="contact-form">
+                    <label htmlFor="name">Full Name</label>
+                    <input type="text" name="name" placeholder="Enter Your Full Name" />
+                    <label htmlFor="email">Email Id</label>
+                    <input type="email" name="email" placeholder="Enter Your Email Address" />
+                    <label htmlFor="message">Message</label>
+                    <textarea name="message" id="message" cols="30" rows="10" placeholder="Enter Your Message" required></textarea>
+                    <button id="submit">Send</button>
+                </form>
+           </div> 
+        </div>
+    )
+}
+
+export default Contact
